1e86e3040a56e917ff0852d9b62d76745a5a5f4d
Sanders DeNardi <sedenardi@gmail.com>
2018-05-25 20:45:44 -0400

    add file stream processing

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const parse = require('./lib/parse');
+const fs = require('fs');
+const MeshTransform = require('./lib/stream');
 
-module.exports = {
-  parseRecord: parse.parseRecord
+module.exports = function(opts) {
+  const file = fs.createReadStream(opts.xmlPath);
+  const transform = new MeshTransform();
+  return file.pipe(transform);
 };
